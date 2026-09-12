c0a680264ac7d5ab4705a68442bb65837c35c4fd
Engmohamed-ali05 <bad100899@gmail.com>
2026-09-12 17:14:59 +0300

    Question 1

--- assignment05/Program.cs
+++ assignment05/Program.cs
@@ -1,10 +1,69 @@
-﻿namespace assignment05
+﻿using System.Drawing;
+
+namespace assignment05
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Q1 
+            ///   Write a method bool TryGetPrice(string title, out double price) that returns 
+           //true and sets price to 25.5 if title is "Clean Code", otherwise returns false and
+           // sets price to 0.Call it and print the price if found
+            
+            static bool trygetprice(string Title, out double Price)
+            {
+                if (Title == "Clean Code")
+
+                {
+                    Price = 25.5;
+                    return true;
+                }
+                else
+                {
+                    Price = 0;
+                    return false;
+                }
+            }
+            static void Main(string[] args)
+
+            {
+
+                double price; 
+                if (trygetprice("cleancode", out price)) 
+                { 
+                    Console.WriteLine(price);
+                }
+
+            }
+
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
