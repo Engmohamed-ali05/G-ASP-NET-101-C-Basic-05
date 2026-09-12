4c4b6804519a1e70bab1e9f9a61a092767e2036f
Engmohamed-ali05 <bad100899@gmail.com>
2026-09-12 17:26:46 +0300

    question1

--- assignment05/Program.cs
+++ assignment05/Program.cs
@@ -13,7 +13,7 @@ namespace assignment05
             //true and sets price to 25.5 if title is "Clean Code", otherwise returns false and
             // sets price to 0.Call it and print the price if found
 
-            /*  static bool trygetprice(string Title, out double Price)
+              static bool trygetprice(string Title, out double Price)
               {
                   if (Title == "Clean Code")
 
@@ -38,52 +38,12 @@ namespace assignment05
                   }
 
 
-              }*/
+              }
 
             #endregion
 
 
-            #region Q2 
-            ///-Write a method PrintBookInfo(string title, int pages = 300) where pages is 
-            //optional.Call it once with only a title, and once passing both a title and pages. 
-
-
-            static void Printbookinfo(string Title, int Pages = 300) 
-            {
-                
-                Console.WriteLine($"title: {Title}, pages: {Pages}");
            
-               
-            
-            }
-
-            static void Main(string[] args2)
-                {
-
-                Printbookinfo("cleancode"); 
-                
-                Printbookinfo("C programming", 600);
-
-
-
-
-
-            }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            #endregion
 
 
 
