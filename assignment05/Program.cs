8c93900d6a34569f5638c765224c5fec109a85a1
Engmohamed-ali05 <bad100899@gmail.com>
2026-09-12 17:24:53 +0300

    Question 2

--- assignment05/Program.cs
+++ assignment05/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Reflection;
 
 namespace assignment05
 {
@@ -9,38 +10,80 @@ namespace assignment05
 
             #region Q1 
             ///   Write a method bool TryGetPrice(string title, out double price) that returns 
-           //true and sets price to 25.5 if title is "Clean Code", otherwise returns false and
-           // sets price to 0.Call it and print the price if found
-            
-            static bool trygetprice(string Title, out double Price)
-            {
-                if (Title == "Clean Code")
+            //true and sets price to 25.5 if title is "Clean Code", otherwise returns false and
+            // sets price to 0.Call it and print the price if found
 
-                {
-                    Price = 25.5;
-                    return true;
-                }
-                else
-                {
-                    Price = 0;
-                    return false;
-                }
-            }
-            static void Main(string[] args)
+            /*  static bool trygetprice(string Title, out double Price)
+              {
+                  if (Title == "Clean Code")
 
-            {
+                  {
+                      Price = 25.5;
+                      return true;
+                  }
+                  else
+                  {
+                      Price = 0;
+                      return false;
+                  }
+              }
+              static void Main(string[] args)
 
-                double price; 
-                if (trygetprice("cleancode", out price)) 
-                { 
-                    Console.WriteLine(price);
-                }
+              {
 
-            }
+                  double price; 
+                  if (trygetprice("cleancode", out price)) 
+                  { 
+                      Console.WriteLine(price);
+                  }
 
+
+              }*/
 
             #endregion
 
+
+            #region Q2 
+            ///-Write a method PrintBookInfo(string title, int pages = 300) where pages is 
+            //optional.Call it once with only a title, and once passing both a title and pages. 
+
+
+            static void Printbookinfo(string Title, int Pages = 300) 
+            {
+                
+                Console.WriteLine($"title: {Title}, pages: {Pages}");
+           
+               
+            
+            }
+
+            static void Main(string[] args2)
+                {
+
+                Printbookinfo("cleancode"); 
+                
+                Printbookinfo("C programming", 600);
+
+
+
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            #endregion
 
 
 
